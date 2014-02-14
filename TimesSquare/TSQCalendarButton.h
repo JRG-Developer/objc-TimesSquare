91b24c2f67e1cab4536a2547e4e8b17fbf6b736e
Joshua Greene <josh@app-order.com>
2014-02-13 18:22:12 -0800

    Added TSQCalenderButton protocol

---
+++ TimesSquare/TSQCalendarButton.h
@@ -0,0 +1,14 @@
+//
+//  TSQCalendarButton.h
+//  AOCore
+//
+//  Created by Joshua Greene on 7/14/13.
+//  Copyright (c) 2013 App-Order. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol TSQCalendarButton <NSObject>
+@optional
+- (void)setItemImage:(UIImage *)image;
+@end
